f54cd075e968584cb08c57b586491b57741eb94b
Tbfaldhi <tubagusfaldhi@gmail.com>
2018-05-10 13:46:21 +0700

    populate

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -1,16 +1,18 @@
 const jwt = require('jsonwebtoken')
 const {Task} = require('../model/todomodel')
+const  { User }  = require('../model/usermodel')
 const mongoose = require('mongoose')
 
 module.exports = {
 
         addTask:function(req,res){
             //res.send('masuk')
-            const token = req.headers.token
-            let decoded = jwt.verify(token, 'SECRET')
-            const task = req.body.task
+            let token  = req.headers.token
+            //Task.users.dropIndexes()
+            let decoded  = jwt.verify(token, 'SECRET')
+            //console.log(decoded)
             let taskList = new Task({
-                user_id: decoded.id,
+                userId: decoded.id,
                 task: req.body.task,
                 status: 'uncomplete'
             })
@@ -21,7 +23,7 @@ module.exports = {
                     res.status(201).json({
                         message: 'task successed add',
                         data: result
-                })
+                    })
 
                 } else {
                     res.status(500).json({
@@ -34,7 +36,20 @@ module.exports = {
     },
 
     getlist:function (req,res) {
-        res.send('web berhasil kami hack')
-    }
+        let token  = req.headers.token
+        let decoded  = jwt.verify(token, 'SECRET')
+        token.id = decoded.id
+        console.log(decoded);
+        
+        Task.
+            find({ userId: decoded.id }).
+                populate('User').
+                exec(function (err, todo) {
+                if (err) console.log(err)
+                res.status(200).json(todo)
+            });
+    },
+
+    
 
 }
